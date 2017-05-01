638cd8f10cd411632ae72e2ac5451532a25bc951
dtaylor31906 <dt01812@georgiasouthern.edu>
2017-04-30 22:32:36 -0400

    appointment classs update

--- app/src/main/java/novapplications/goodlooks/models/Appointment.java
+++ app/src/main/java/novapplications/goodlooks/models/Appointment.java
@@ -1,5 +1,6 @@
 package novapplications.goodlooks.models;
 
+import java.util.AbstractList;
 import java.util.Date;
 
 
@@ -9,9 +10,57 @@ import java.util.Date;
 
 public class Appointment
 {
-    User provider,customer;
-    Date startTime,estimatedEndTime, actualEndTime;
-    Service service[];
+    private User provider,customer;
+    private Date startTime, estimatedEndTime, actualEndTime;
+    private AbstractList<Service> servicesToBePerformed;//services to be performed.
+
+
+    public Appointment() {
+    }
+
+    public AbstractList<Service> getServicesToBePerformed() {
+        return servicesToBePerformed;
+    }
+
+    public void setServicesToBePerformed(AbstractList<Service> servicesToBePerformed) {
+        this.servicesToBePerformed = servicesToBePerformed;
+    }
+
+    public User getProvider() {
+        return provider;
+    }
+
+    public void setProvider(User provider) {
+        this.provider = provider;
+    }
+
+    public User getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(User customer) {
+        this.customer = customer;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEstimatedEndTime() {
+        return estimatedEndTime;
+    }
+
+    public void setEstimatedEndTime(Date estimatedEndTime) {
+        this.estimatedEndTime = estimatedEndTime;
+    }
+
+    public Date getActualEndTime() {
+        return actualEndTime;
+    }
+
+    public void setActualEndTime(Date actualEndTime) {
+        this.actualEndTime = actualEndTime;
+    }
 
     public void getDescription() {
 
