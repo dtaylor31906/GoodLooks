fe19cc2282bb15c87ebd88dfe4bdd8e0e0b7ef47
Kenneth Smith <kenneths653@yahoo.com>
2017-05-01 03:11:30 -0400

    Added New Customer_Stylist blank Activity

---
+++ app/src/main/java/novapplications/goodlooks/CustomerStylistPicker2.java
@@ -0,0 +1,13 @@
+package novapplications.goodlooks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerStylistPicker2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_stylist_picker2);
+    }
+}
